422c77f247e83cdb579bc55191913e1e3fafd647
doh7405 <70460468+doh7405@users.noreply.github.com>
2020-09-09 21:37:27 +0900

    second commit 이번에는 좀 많이 함

--- among us_temperature in python.py
+++ among us_temperature in python.py
@@ -1,8 +1,28 @@
 from tkinter import *
+import random
 
 win = Tk()
 win.title('temperature mission')
 win.geometry('300x250')
 win.resizable(width=False, height=True)
+
+text1 = random.randint(-32, -20)
+
+def btnpress():
+    global text1
+    text1 += 1
+    print(text1)
+
+
+tp = Label(win, text='temperature', bg='yellow')
+tp.pack()
+
+a = Frame(win)
+a.pack()
+aim = Label(a, text='aim')
+aim.pack()
+upbtn = Button(a, text='up', command=btnpress)
+upbtn.pack()
+view1 = Label(a, text=text1)
 
 win.mainloop()
